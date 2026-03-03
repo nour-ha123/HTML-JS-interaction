5759a3d7b310cb6563cd29000b72ca79f96f86f1
nour-ha123 <hanafi.nour11@gmail.com>
2026-03-03 21:04:46 +0100

    test

--- script.js
+++ script.js
@@ -14,6 +14,24 @@ var paragrathe = document.getElementsByTagName('p')[0];
   imgT.style.height = "300px"
 
 
+
+  var image = document.querySelector('img');
+  var img1 = "https://fastly.picsum.photos/id/0/5000/3333.jpg?hmac=_j6ghY5fCfSD6tvtcV74zXivkJSPIfR9B8w34XeQmvU";
+  var img2 = "https://fastly.picsum.photos/id/7/4728/3168.jpg?hmac=c5B5tfYFM9blHHMhuu4UKmhnbZoJqrzNOP9xjkV4w3o";
+  
+  function alterner() {
+    if (image.src.includes("")) {
+        image.src = img2;
+    } else {
+        image.src = img1;
+    }
+}
+
+image.addEventListener('click', function() {
+    alterner();
+});
+setInterval(alterner, 3000);
+
   var listes = document.querySelectorAll('li');
 
   listes.forEach(function(li) {
